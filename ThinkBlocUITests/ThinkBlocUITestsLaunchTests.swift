bd836bce51b03b16fbb3e36d76ff8ff8dd488ea1
Noah Syanahan <noahsyanahan@gmail.com>
2022-07-25 14:12:10 +0700

    Initial Commit

---
+++ ThinkBlocUITests/ThinkBlocUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ThinkBlocUITestsLaunchTests.swift
+//  ThinkBlocUITests
+//
+//  Created by Noah S on 25/07/22.
+//
+
+import XCTest
+
+class ThinkBlocUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
